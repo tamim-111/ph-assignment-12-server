a627bc8942a6f862121f0cb1daedbc8d86396acc
tamim-111 <tamim144362@gmail.com>
2025-07-19 03:18:26 +0600

    feat: vercel deployment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4000
 const app = express()
 // middleware
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://assignment-12-a7eae.web.app'],
     credentials: true,
     optionSuccessStatus: 200,
 }
@@ -382,10 +382,10 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db('admin').command({ ping: 1 })
-        console.log(
-            'Pinged your deployment. You successfully connected to MongoDB!'
-        )
+        // await client.db('admin').command({ ping: 1 })
+        // console.log(
+        //     'Pinged your deployment. You successfully connected to MongoDB!'
+        // )
     } finally {
         // Ensures that the client will close when you finish/error
     }
